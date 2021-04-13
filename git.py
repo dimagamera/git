e5f42709d01e361c05eb56dbf8a56ea9b782f2f2
dimagamera <46674070+dimagamera@users.noreply.github.com>
2021-04-13 14:03:59 +0000

    Update git.py

--- git.py
+++ git.py
@@ -1,12 +1,10 @@
 import requests
 import json
 from github import Github
-# g = Github()
-# user = g.get_user('dimagamera')
-# for repo in user.get_repos():
-#     print(repo)
-# r = requests.get('https://api.github.com/search/users?q=location:ukraine').json()
-# for item in r['items']:
-# 	print("https://github.com/"+item['login'])
-s = requests.get('https://api.github.com/users/USERNAME/repos').json()
-print(s)
+g = Github()
+user = g.get_user('dimagamera')
+for repo in user.get_repos():
+  print(repo)
+r = requests.get('https://api.github.com/search/users?q=location:ukraine').json()
+for item in r['items']:
+  print("https://github.com/"+item['login'])
