7f7cdbab909606d782c0940de19d6fdfc61281c4
dimagamera <46674070+dimagamera@users.noreply.github.com>
2021-04-14 14:09:46 +0000

    Update git.py

--- git.py
+++ git.py
@@ -8,17 +8,25 @@ if main == 1:
 	username = input('Enter username > ')
 	g = Github()
 	user = g.get_user('dimagamera')
+	i = 0
 	for repo in user.get_repos():
+	    i +=1
 	    print(repo)
+	print('total count repositories: '+str(i))
 elif main == 2:
 	name_country = input('Enter name country > ')
 	r = requests.get('https://api.github.com/search/users?q=location:'+ name_country).json()
+	i = 0
 	for item in r['items']:
+		i +=1
 		print("https://github.com/"+item['login'])
+	print('total count users in '+name_country +': '+str(i))
 elif main == 3:
 	language = input('Enter language > ')
 	r = requests.get('https://api.github.com/search/repositories?q=language:'+language+'&per_page=100').json()
 	i = 0
-	while i < 100:
+	while i < 99:
 		i+=1
 		print(r['items'][i]['html_url'])
+	i = i+1
+	print('total count repositories '+language+': '+str(i))
